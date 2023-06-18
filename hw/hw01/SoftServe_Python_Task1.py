51d9e3799c78c618415a1158be1768b092b421c9
Vadym <70985297+Farlenski@users.noreply.github.com>
2023-06-18 21:07:42 +0300

    Add files via upload

---
+++ hw/hw01/SoftServe_Python_Task1.py
@@ -0,0 +1,8 @@
+#task1
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+city = input("Where do you live?: ")
+
+print(f"Hello, {name}\nYour age is {age}\nYou live in {city}")
+
+                    
