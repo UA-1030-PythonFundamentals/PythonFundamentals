c7a3e2bd032ba8b3f8449659012d1bfec69fa19e
YevhenDuhin <duginevgen@gmail.com>
2023-06-19 01:35:45 +0300

    YevhenDuhin

---
+++ hw/hw01/YevhenDuhin/hw01_task02.py
@@ -0,0 +1,18 @@
+a = int(input("Введіть перше число (A): "))
+b = int(input("Введіть друге число (B): "))
+
+sum = a + b
+sum2 = a - b
+sum3 = a * b
+sum4 = a / b
+sum5 = a ** b
+sum6 = a // b
+sum7 = a % b
+
+print("Додавання: ", sum)
+print("Віднімання: ", sum2)
+print("Множиння: ", sum3)
+print("Ділення: ", sum4)
+print("Зведення в ступінь: ", sum5)
+print("Ділення із округленням: ", sum6)
+print("Залишок від ділення: ", sum7)
