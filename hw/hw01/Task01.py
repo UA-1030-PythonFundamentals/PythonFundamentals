9e46870fb87f20127f797e5b65b6fcd5a77f46b5
BohdanBiriuk <136360389+BohdanBiriuk@users.noreply.github.com>
2023-06-20 11:08:42 +0300

    Add files via upload

---
+++ hw/hw01/Task01.py
@@ -0,0 +1,7 @@
+a = input("What is your name?: ")
+b = input("How old are you?: ")
+c = input("where do you live?: ")
+
+print("Hi! Nice to meet you,", a)
+print("Your age is,", b)
+print("You live in,", c)
