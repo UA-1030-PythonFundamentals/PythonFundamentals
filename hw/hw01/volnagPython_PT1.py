e4dc7ea3a47f9baec486d9aeb24843833eee7231
Volodymyr Nahirniak <volnag@gmail.com>
2023-06-21 20:21:02 +0300

    Added files inro directory

--- hw/hw01/volnagPython_PT1.py
+++
@@ -1,8 +0,0 @@
-#June 14, 2023- PT1
-nm = input('What is your name?')
-print(' Hello', nm,'!')
-ag = input('How old are you?')
-print(' You are ', ag, 'years old.')
-ct = input('Where do you live?')
-print(' I am from ', ct,'.')
-#VN
