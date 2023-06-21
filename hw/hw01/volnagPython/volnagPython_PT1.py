09e196401f50cd9c4ad31e100c6c1476c191052a
Volodymyr Nahirniak <volnag@gmail.com>
2023-06-21 20:22:46 +0300

    Created directory with my name

---
+++ hw/hw01/volnagPython/volnagPython_PT1.py
@@ -0,0 +1,8 @@
+#June 14, 2023- PT1
+nm = input('What is your name?')
+print(' Hello', nm,'!')
+ag = input('How old are you?')
+print(' You are ', ag, 'years old.')
+ct = input('Where do you live?')
+print(' I am from ', ct,'.')
+#VN
