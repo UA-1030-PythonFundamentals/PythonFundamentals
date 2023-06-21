60567b0e460a3974b37b6abef61742400d5107a7
YevhenDuhin <duginevgen@gmail.com>
2023-06-21 14:56:51 +0300

    YevhenDuhin

--- hw/hw01/YevhenDuhin/hw01_task02.py
+++ hw/hw01/YevhenDuhin/hw01_task02.py
@@ -1,7 +1,7 @@
 a = int(input("Введіть перше число (A): "))
 b = int(input("Введіть друге число (B): "))
 
-sum = a + b
+sum1 = a + b
 sum2 = a - b
 sum3 = a * b
 sum4 = a / b
@@ -9,10 +9,10 @@ sum5 = a ** b
 sum6 = a // b
 sum7 = a % b
 
-print("Додавання: ", sum)
+print("Додавання: ", sum1)
 print("Віднімання: ", sum2)
 print("Множиння: ", sum3)
 print("Ділення: ", sum4)
-print("Зведення в ступінь: ", sum5)
+print("Піднесення до степені: ", sum5)
 print("Ділення із округленням: ", sum6)
 print("Залишок від ділення: ", sum7)
---
+++ hw/hw03/YevhenDuhin/hw3_task02/task02.py
@@ -0,0 +1,13 @@
+number = 1987
+
+product = 0.3
+for digit in str(number):
+    product *= int(digit)
+
+print("The product of the digits of this number:", type(product), product)
+
+revers_number = int(str(number)[::-1])
+print("The number in reverse order:", revers_number, type(revers_number))
+
+sorted_digits = sorted((int(digit) for digit in str(number)))
+print("In ascending order:", sorted_digits, type (sorted_digits))
