d3ac8e5d7b9e356e8216fe0baa1d5d38a6a3abc9
Volodymyr Nahirniak <volnag@gmail.com>
2023-07-04 17:08:35 +0300

    Added the PT9 and PT10 from HW6 to directory hw06/volnagPython locally

---
+++ hw/hw06/volnagPython/volnagPython_PT9.py
@@ -0,0 +1,29 @@
+##Task1. In the range from 1 to 10 determine
+##• even numbers that are divisible by 2,
+##• odd numbers, which are divisible by 3,
+##• numbers that are not divisible by 2 and 3.--HW9-- July 4, 2023
+
+num = list(range(1,11,1))
+odd_num = list()
+even_num = list()
+unq_num = []
+
+print("Initial set of numbers =>", num)
+
+for k in num:
+
+  if not(k%2 == 0) and not(k%3 == 0):
+        unq_num.append(k)
+  elif k%3 == 0 and not(k%2 == 0):
+        odd_num.append(k)
+  elif k%2 == 0:
+        even_num.append(k)
+
+  else:
+        print("-----No required numbers!-----")
+print(70*("*"))
+print("Our even numbers :",even_num)
+print("Our odd numbers :",odd_num)
+print("Prime numbers :",unq_num)
+print(70*("*"))
+#VN
