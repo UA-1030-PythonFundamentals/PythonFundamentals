0bb489f5aa1b564ee19b88963f294a26ee4d6e2b
Volodymyr Nahirniak <volnag@gmail.com>
2023-07-13 09:36:57 +0300

    Finished HW08. Added files to hw/hw08/volnagPython locally

---
+++ hw/hw08/volnagPython/calculator.py
@@ -0,0 +1,28 @@
+#Module containing functions
+#which perform basic
+#arithmetic operartions - calculator.py
+
+import math
+
+
+def ad(n1,n2):
+    '''Calculates addition of two numbers'''
+    s = n1 + n2
+    return s
+
+def prd(n1,n2):
+    '''Calculates multiplication of two numbers'''
+    m = n1 * n2
+    return m
+
+def dvs(n1,n2):
+    '''Calculates division of two numbers'''
+    f = n1/n2
+    return f
+
+def sbt(n1,n2):
+    '''Calculates subtration of two numbers'''
+    d = n1-n2
+    return d
+
+#VN
