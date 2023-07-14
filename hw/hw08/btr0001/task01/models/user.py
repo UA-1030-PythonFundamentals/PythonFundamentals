02005c05c7a58f208689a6a041de08f984d97110
Taras Boichuk <btr0001@ukr.net>
2023-07-14 22:30:15 +0300

    Added tasks

---
+++ hw/hw08/btr0001/task01/models/user.py
@@ -0,0 +1,5 @@
+__all__ = ["create_user"]
+
+
+def create_user():
+    pass
---
+++ hw/hw08/btr0001/task01/utils/__init__.py
@@ -0,0 +1,6 @@
+from .formatter import *
+from .logger import *
+
+__all__ = ["format_string", "log_in_file"]
+
+# print(dir())
