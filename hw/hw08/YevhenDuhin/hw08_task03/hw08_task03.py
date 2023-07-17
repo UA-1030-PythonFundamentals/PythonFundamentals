8238aecf9fbe5c13c37b1b533126d1592f4b717b
YevhenDuhin <duginevgen@gmail.com>
2023-07-17 16:21:33 +0300

    YevhenDuhin hw08

---
+++ hw/hw08/YevhenDuhin/hw08_task03/hw08_task03.py
@@ -0,0 +1,10 @@
+import math
+
+def calculate_rectangle_area(a, b):
+    return a * b
+
+def calculate_triangle_area(a, h):
+    return 0.5 * a * h
+
+def calculate_circle_area(r):
+    return math.pi * math.pow(r, 2)
