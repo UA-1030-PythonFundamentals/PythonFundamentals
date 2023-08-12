c8fb20e51019a46995a4731f4a8157438a4511e5
Olha Zhovnir <olyamazyrenko@gmail.com>
2023-08-11 21:01:40 -0500

    Added HW08

---
+++ hw/hw08/Olha Zhovnir/Task3/formulas_for_area.py
@@ -0,0 +1,9 @@
+import math
+def rectangle_area(a, b):
+    print(round(a*b, 2))
+
+def triangle_area(h,a):
+    print(round(0.5*h*a, 2))
+
+def circle_area(r):
+    print(round(math.pi*math.pow(r, 2), 2))
