af2acd5ca96f75e21b5a5c94271872632874ca9d
volnagPython <volnag@gmail.com>
2023-08-29 17:14:37 +0300

    Updated files w/ my project

--- degree_projects/volnagPython/Zodiac/zodiac.py
+++ degree_projects/volnagPython/Zodiac/zodiac.py
@@ -5,72 +5,75 @@ def zod(month_in, date_in):
 
     global sign
     sign = None
+    mlist=["Січень", "Лютий", "Березень", "Квітень",
+            "Травень","Червень","Липень","Серпень",
+            "Вересень","Жовтень","Листопад","Грудень"]
+
     
-    if month_in == "Ma" and date_in in range(22,32):
+    if month_in == "Березень" and date_in in range(22,32):
         sign = "Овен"
-    elif month_in == "A" and date_in in range(1,20):
+    elif month_in == "Квітень" and date_in in range(1,20):
         sign = "Овен"
 
-    elif month_in == "A" and date_in in range(20,31):
+    elif month_in == "Квітень" and date_in in range(20,31):
         sign = "Телець"
-    elif month_in == "My" and date_in in range(1,21):
+    elif month_in == "Травень" and date_in in range(1,21):
         sign = "Телець"
 
-    elif month_in == "My" and date_in in range(21,32):
+    elif month_in == "Травень" and date_in in range(21,32):
         sign = "Близнюки"
-    elif month_in == "Ju" and date_in in range(1,21):
+    elif month_in == "Червень" and date_in in range(1,21):
         sign = "Близнюки"
 
-    elif month_in == "Ju" and date_in in range(21,32):
+    elif month_in == "Червень" and date_in in range(21,31):
         sign = "Рак"
-    elif month_in == "Jy" and date_in in range(1,23):
+    elif month_in == "Липень" and date_in in range(1,23):
         sign = "Рак"
 
-    elif month_in == "Jy" and date_in in range(23,32):
+    elif month_in == "Липень" and date_in in range(23,32):
         sign = "Лев"
-    elif month_in == "Ag" and date_in in range(1,23):
+    elif month_in == "Серпень" and date_in in range(1,23):
         sign = "Лев"
 
-    elif month_in == "Ag" and date_in in range(23,32):
+    elif month_in == "Серпень" and date_in in range(23,32):
         sign = "Діва"
-    elif month_in == "S" and date_in in range(1,23):
+    elif month_in == "Вересень" and date_in in range(1,23):
         sign = "Діва"
         
-    elif month_in == "S" and date_in in range(23,31):
+    elif month_in == "Вересень" and date_in in range(23,31):
         sign = "Терези"
-    elif month_in == "O" and date_in in range(1,23):
+    elif month_in == "Жовтень" and date_in in range(1,23):
         sign = "Терези"
     
-    elif month_in == "O" and date_in in range(23,32):
+    elif month_in == "Жовтень" and date_in in range(23,32):
         sign = "Скорпіон"
-    elif month_in == "N" and date_in in range(1,22):
+    elif month_in == "Листопад" and date_in in range(1,22):
         sign = "Скорпіон"
 
-    elif month_in == "N" and date_in in range(22,31):
+    elif month_in == "Листопад" and date_in in range(22,31):
         sign = "Змієносець"
-    elif month_in == "D" and date_in in range(1,22):
+    elif month_in == "Грудень" and date_in in range(1,22):
         sign = "Змієносець"
         
-    elif month_in == "J" and date_in in range(1,20):
+    elif month_in == "Січень" and date_in in range(1,20):
         sign = "Козоріг"
-    elif month_in == "D" and date_in in range(22,32):
+    elif month_in == "Грудень" and date_in in range(22,32):
         sign = "Козоріг"
         
-    elif month_in == "J" and date_in in range(20,32):
-        sign = "Водолій "
-    elif month_in == "F" and date_in in range(1,19):
-        sign = "Водолій "
+    elif month_in == "Січень" and date_in in range(20,32):
+        sign = "Водолій"
+    elif month_in == "Лютий" and date_in in range(1,19):
+        sign = "Водолій"
         
-    elif month_in == "F" and date_in in range(20,30):
+    elif month_in == "Лютий" and date_in in range(20,30):
         sign = "Риби"
-    elif month_in == "Ma" and date_in in range(1,21):
+    elif month_in == "Березень" and date_in in range(1,21):
         sign = "Риби"
     elif sign == None:
         sign = "Будь-ласка, введіть правильну дату..."
     return sign
 
-##zod("F",32)
-##print(sign)
+
 ##Aries star sign dates: March 21 – April 19
 ##Taurus star sign dates: April 20 – May 20
 ##Gemini star sign dates: May 21 – June 20
@@ -84,5 +87,5 @@ def zod(month_in, date_in):
 ##Aquarius star sign dates: January 20 – February 18
 ##Pisces star sign dates: February 19 – March 20
 
-
+#--August 29, 2023 --VN
 
